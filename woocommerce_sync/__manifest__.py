1fd9acc813f38d92ea172831960f9368c74cb517
roboes <rg.roberto@outlook.com>
2026-09-13 23:45:52 +0200

    Update

--- woocommerce_sync/__manifest__.py
+++ woocommerce_sync/__manifest__.py
@@ -6,9 +6,9 @@
     'author': 'roboes',
     'website': 'https://github.com/roboes/odoo-woocommerce-sync',
     'category': 'Connectors',
-    # 'version': '16.0.5.5',
-    # 'version': '18.0.5.5',
-    'version': '19.0.5.5',
+    # 'version': '16.0.5.6',
+    # 'version': '18.0.5.6',
+    'version': '19.0.5.6',
     'external_dependencies': {
         'python': ['filetype', 'phonenumbers', 'woocommerce'],
     },
